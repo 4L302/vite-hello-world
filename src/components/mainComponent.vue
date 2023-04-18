0495d20d7928d7d703f1c6de12812574b85fb28c
4L302 <alegarg2002@gmail.com>
2023-04-18 14:55:58 +0200

    .

--- src/components/mainComponent.vue
+++ src/components/mainComponent.vue
@@ -6,116 +6,7 @@
                 <a href="#">Current series</a>
             </div>
             <div class="row pt-5">
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/action-comics.jpg" alt="Action Comics">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Action Comics</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/american-vampires-1976.jpg" alt="American Vampire 1976">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">American Vampire 1976</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/acquaman.webp" alt="Acquaman">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Acquaman</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batgirl.jpg" alt="Batgirl">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Batgirl</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman.jpg" alt="Batman">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Batman</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-beyoind.jpg" alt="Batman beyoind">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Batman Beyoind</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-superman.jpg" alt="Batman/Superman">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Batman/Superman</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-superman-annual.jpg" alt="Batman/Superman Annual">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">Batman/Superman Annual</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-the-joker-war-zone.jpg"
-                            alt="BATMAN: THE JOKER WAR ZONE">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">BATMAN: THE JOKER WAR ZONE</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-three-jokers.jpg" alt="BATMAN: THREE JOKERS">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">BATMAN: THREE JOKERS</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/batman-harley-queen.jpg"
-                            alt="BATMAN: WHITE KNIGHT PRESENTS: HARLEY QUINN">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">BATMAN: WHITE KNIGHT PRESENTS: HARLEY QUINN</span>
-                    </div>
-                </div>
-                <div
-                    class="col-xxl-2 col-lg-3 col-md-4 col-sm-12 col-12 d-flex d-sm-block justify-content-center align-items-center">
-                    <div class="comic-card mt-3 d-flex flex-column justify-content-center align-items-center d-sm-block">
-                        <img class="cover" src="../assets/img/catwoman.jpg" alt="CATWOMAN">
-                    </div>
-                    <div class="text-cover text-center text-sm-start">
-                        <span class="text-white text-uppercase mt-3">CATWOMAN</span>
-                    </div>
-                </div>
+                <cardComponent />
             </div>
             <div class="load text-center d-flex justify-content-center text-uppercase">
                 <span>Load more</span>
@@ -151,8 +42,13 @@
 </template>
 
 <script>
+
+import cardComponent from './cardComponent.vue';
 export default {
-    name: 'mainComponent'
+    name: 'mainComponent',
+    components:{
+        cardComponent
+    }
 }
 </script>
 
